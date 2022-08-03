67b36b204ee4207a305f211616bdec119974ea29
beatricekallen <bkallen90@gmail.com>
2022-08-02 22:06:56 -0400

    updates about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,20 @@
 const About = () => {
   return (
     <div>
-      <p>about me</p>
+      <h1>Welcome!</h1>
+      <p>
+        Thanks for checking out my photography! I have always loved photography,
+        but in 2017 I started shooting seriously with a Nikon F2 Photomic and
+        fell in love with film.
+      </p>
+      <p>
+        I love shooting around the Triangle on weekends and planning trips to
+        new desinations. The ocean is one of my favorite subjects.
+      </p>
+      <p>
+        Please feel free to contact me at bkallen90@gmail.com if you'd like to
+        get in touch!
+      </p>
     </div>
   );
 };
